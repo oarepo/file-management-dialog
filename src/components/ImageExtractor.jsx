4fb56ca87694b2a40ef8ec11539ba9c2962b8d4f
Martin Čorovčák <martino.coro@gmail.com>
2023-07-26 14:53:11 +0200

    Release memory with images=[imageSources]

--- src/components/ImageExtractor.jsx
+++ src/components/ImageExtractor.jsx
@@ -23,7 +23,10 @@ const ImageExtractor = () => {
           return;
         }
         console.log(event.data);
-        setImages(images => [...images, event.data]);
+        const imageSrc = URL.createObjectURL(
+          new Blob([event.data.imageData], { type: `image/${event.data.imageType}` })
+        );
+        setImages((images) => [...images, imageSrc]);
       };
     } else {
       console.log("Web Worker is not supported");
@@ -45,6 +48,8 @@ const ImageExtractor = () => {
       const data = await file.arrayBuffer();
       extractImageWorker.postMessage(data, [data]);
       isProcessing.current = true;
+      // Revoke and Set
+      images.forEach((imageSrc) => URL.revokeObjectURL(imageSrc));
       setImages([]);
     } else {
       console.log("Web Worker is not supported");
@@ -56,23 +61,35 @@ const ImageExtractor = () => {
       <input type="file" onChange={handleFileChange} />
       <div>{file && `${file.name} - ${file.type}`}</div>
       <button onClick={handleUploadClick}>Upload</button>
-      <div style={{
-        display: "grid",
-        gridColumnGap: "10px",
-        gridRowGap: "10px"
-      }}>
-        {images.map((image, index) => (
-          <img
-            key={index}
-            src={URL.createObjectURL(
-              new Blob([image.imageData], { type: `image/${image.imageType}` })
-            )}
-            width="50%"
-            style={{ display: "inline-block" }}
-          />
-        ))}
+      <div
+        style={{
+          display: "grid",
+          gridColumnGap: "10px",
+          gridRowGap: "10px",
+        }}
+      >
+        {images.map((imageSrc) => {
+          // const imageSrc = URL.createObjectURL(
+          //   new Blob([image.imageData], { type: `image/${image.imageType}` })
+          // );
+          // const onLoad = (img) => {
+          //   if (!img) return;
+          //   img.onload = () => URL.revokeObjectURL(imageSrc);
+          //   if (img.complete) URL.revokeObjectURL(imageSrc);
+          // };
+          return (
+            <img
+              src={imageSrc}
+              key={imageSrc}
+              width="50%"
+              style={{ display: "inline-block" }}
+            />
+          );
+        })}
       </div>
-      <button value={clickCount} onClick={() => setClickCount(clickCount + 1)}>{clickCount}</button>
+      <button value={clickCount} onClick={() => setClickCount(clickCount + 1)}>
+        {clickCount}
+      </button>
     </>
   );
 };
